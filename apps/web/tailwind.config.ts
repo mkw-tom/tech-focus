b2be95c5d564e7f619f43c76ac47494fd28e6ceb
Tomu Maekawa <tomu.m1107@icloud.com>
2026-04-11 21:21:37 +0900

    feat(web): add tailwind and daisyui foundation

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,18 @@
+import daisyui from "daisyui"
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["business", "dim"],
+  },
+}
+
+export default config
